e6c72bf240161f41bc1d7f5c1431bac5bc900d75
Joeb3219 <joseph.a.boyle@rutgers.edu>
2017-08-16 23:14:37 -0400

    Fixes verification; adds reset sequences

--- main.c
+++ main.c
@@ -16,6 +16,8 @@
 #define MEMORY_INTERNAL_RAM_END_START 0xFFC0
 #define MEMORY_INTERRUPT_ENABLE_START 0xFFFF
 
+#define MEMORY_STACK_START 0xFFFE
+
 void loadROM(unsigned char *memory, char *fileName){
 	FILE * rom = fopen(fileName, "r");
 	size_t fileSize = 0;
@@ -29,7 +31,47 @@ void loadROM(unsigned char *memory, char *fileName){
 	fclose(rom);
 }
 
+void resetStackAndRegisters(struct cpu_cpu *cpu, unsigned char *memory){
+	cpu->registers->a = 0x0001;
+	cpu->registers->f = 0x00B0;
+	cpu->registers->bc = 0x0013;
+	cpu->registers->de = 0x00D8;
+	cpu->registers->hl = 0x014D;
+	cpu->registers->sp = 0xFFFE;
+	memory[0xFF05] = memory[0xFF06] = memory[0xFF07] = 0x0000;
+	memory[0xFF10] = 0x0080;
+	memory[0xFF11] = 0xBF;
+	memory[0xFF12] = 0xF3;
+	memory[0xFF14] = 0xBF;
+	memory[0xFF16] = 0x3F;
+	memory[0xFF17] = 0x00;
+	memory[0xFF19] = 0xBF;
+	memory[0xFF1A] = 0x7F;
+	memory[0xFF1B] = 0xFF;
+	memory[0xFF1C] = 0x9F;
+	memory[0xFF1E] = 0xBF;
+	memory[0xFF20] = 0xFF;
+
+	memory[0xFF21] = 0x00;
+	memory[0xFF22] = 0x00;
+	memory[0xFF23] = 0xBF;
+	memory[0xFF24] = 0x77;
+	memory[0xFF25] = 0xF3;
+	memory[0xFF26] = 0xF1;
+	memory[0xFF40] = 0x91;
+	memory[0xFF42] = 0x00;
+	memory[0xFF43] = 0x00;
+	memory[0xFF45] = 0x00;
+	memory[0xFF47] = 0xFC;
+	memory[0xFF48] = 0xFF;
+	memory[0xFF49] = 0xFF;
+	memory[0xFF4A] = 0x00;
+	memory[0xFF4B] = 0x00;
+	memory[0xFFFF] = 0x00;
+}
+
 int main(int argc, char** argv){
+	int i, tmp;
 
 	if(argc != 2){
 		printf("Usage: %s <filename>\n", argv[0]);
@@ -51,6 +93,21 @@ int main(int argc, char** argv){
 	loadROM(memory, argv[1]);
 
 	printf("Memory @ 0x000: %0x\n", memory[0x0000]);
+
+	// POWER UP SEQUENCE
+
+	// Verify that rom has valid signature.
+	tmp = 0;
+	for(i = 0x134; i < 0x14d; i ++) tmp += memory[i];
+	if( ( (tmp + 25) & 0x80) != 0x80){
+		printf("Invalid signature.\n");
+		return 1;
+	}
+
+	// Verification complete, we can now load in values to registers for program execution.
+
+
+	resetStackAndRegisters(cpu, memory);
 
 	destroyCPU(cpu);
 
